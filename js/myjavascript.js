ee10ec45d28b3cb0900bbb912ccfb1550042fe28
ec <varikvi@yahoo.com>
2018-04-29 21:52:30 +0200

    small corr; v1

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -4,6 +4,8 @@ $('.port-item').click(function() {
 
 $('[data-toggle="tooltip"]').tooltip();
 
+//https://codepen.io/nxworld/pen/OyRrGy
+//https://css-tricks.com/introducing-css-scroll-snap-points/
 $(function() {
     $('.port-item').on('click', function(e) {
         e.preventDefault();
