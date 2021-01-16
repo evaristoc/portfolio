58c1247871ac1086a404af4eedda40979dec1719
ec <varikvi@yahoo.com>
2021-01-17 00:22:26 +0100

    updates: js for collapsing; adjusting main menu for xs screens; adjusting banner; adjusting size Welcome section

--- js/myjavascript.js
+++ js/myjavascript.js
@@ -1,5 +1,23 @@
+//console.log($('#home')[0])
+var openSection = "#home";
+
 $('.port-item').click(function() {
-    $('.collapse').collapse('hide');
+    //console.log($('.collapse')[0]);
+    //console.log($('#home')[0] === $('.collapse')[0]);
+    //console.log($(this).attr("data-target"));
+    //for(let i = 0; i < 4; i++){
+    //    console.log($(".collapse")[i].id);
+    //    console.log($(".collapse")[i].className.split(/\s+/));
+    //};
+    //console.log($(this).attr("data-target") == openSection);
+    if($(this).attr("data-target") == openSection){
+        event.stopPropagation();
+        return;
+    }else{
+        $('.collapse').collapse('hide');
+        openSection = $(this).attr("data-target");
+    };
+    
 })
 
 $('[data-toggle="tooltip"]').tooltip();
